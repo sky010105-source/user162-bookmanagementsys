e8c64a420b4f584e818b60c2fdd4bc72c7a3e5c4
sky010105-source <sky010105@gmail.com>
2026-06-24 16:50:07 +0900

    add /api

--- backend/src/main/java/com/aivle/bookapp/controller/BookController.java
+++ backend/src/main/java/com/aivle/bookapp/controller/BookController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class BookController {
 
